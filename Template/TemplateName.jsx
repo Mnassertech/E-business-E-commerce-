6e34acc25c742c97d6458b9e26b4abb562692f82
Marwan Nasser <mnasser.tech@gmail.com>
2026-05-15 19:39:18 +0300

    Remove all files

--- Template/TemplateName.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react'
-
-export default function TemplateName() {
-  return (
-    <>
-      <h1>TemplateName</h1>
-    </>
-  )
-}
--- src copy/components/btn/btn.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react'
-
-export default function Btn() {
-  return (
-    <>
-      <h1>Btn</h1>
-    </>
-  )
-}
